0fabdeb2250e2e6209385fd38cddc247fe438109
ineeve <renatoineeve@gmail.com>
2017-03-21 15:23:43 +0000

    Added stat to check if is directory

--- sfind.c
+++ sfind.c
@@ -1,7 +1,17 @@
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdio.h>
 
 
 int main(int argc, char **argv){
+struct stat s;
+const char *pathname = "/";
+stat(pathname,&s);
+if (S_ISDIR(s.st_mode)){
+printf("Directory\n");
+}
+
 if (argc == 2){
 	execlp("ls","ls",argv[1],NULL);
 }
