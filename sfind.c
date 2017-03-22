89c56dc4a72d3065a9aeea899f0714e7a3c2b469
João Carvalho <joaocarvalho9@gmail.com>
2017-03-22 10:59:24 +0000

    add comments about functions

--- sfind.c
+++ sfind.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
 
-
 int main(int argc, char **argv){
-if (argc == 2){
-	execlp("ls","ls",argv[1],NULL);
-}
-
-
-
+  if (argc == 2){
+	   execlp("ls","ls",argv[1],NULL);
+  }
   return 0;
 }
+
+
+//readdir
+//opendir
+//rewinddir??
+//sisdir
