4b44f59c8e3f2e116b7c9110124165d585a959f3
ineeve <renatoineeve@gmail.com>
2017-04-11 19:14:05 +0100

    This version is working better

--- sfind.c
+++ sfind.c
@@ -5,6 +5,7 @@
 #include <dirent.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <string.h>
 
 //handler CRTL-C
 void sigint_handler(int signo)
@@ -44,39 +45,57 @@ int main(int argc, char **argv){
 		printf("This program requires at least 1 argument (directory), example ./sfind / \n");
 	}
 	DIR *directory;
-	directory = opendir(argv[1]);
-	struct dirent *dire;
-	struct stat s;
-	while((dire = readdir(directory)) != NULL){
-		printf("%s->",(*dire).d_name);
-		stat((*dire).d_name,&s);
-		switch(s.st_mode & S_IFMT){
-		case(S_IFREG):
-				printf("Regular file;\n ");
-		break;
-		case (S_IFDIR):
-				printf("Directory;\n ");
-		char *dirName = (*dire).d_name;
-		if (dirName[0] == '.'){
-			if (dirName[1] == '\0' || dirName[1] == '.'){
-				break;
+	chdir(argv[1]);
+	if ((directory = opendir(".")) == NULL){
+		perror("Error Reading Dir\n");
+	}
+	struct dirent *file;
+	struct stat file_info;
+	char dirsFound[100][1024]; //Space for 99dirs 1023 bytes long each
+	int dirsIterator = 0;
+	int i;
+	pid_t parentpid = getpid();
+	char cwd[1024];
+	if( getcwd(cwd,sizeof(cwd)) == NULL){
+		perror("Error reading cwd\n");
+	}
+	while((file = readdir(directory)) != NULL){
+		char *fileName = (*file).d_name;
+		printf("Current file name: %s\n",fileName);
+		if (stat(fileName,&file_info)==-1){
+			printf("Failed to open directory %s\n", argv[1]);
+			perror("stat");
+			return 1;
+		}
+		if (S_ISDIR(file_info.st_mode)){
+			if (fileName[0] == '.' && (fileName[1] == '\0' || fileName[1] == '.')){
+				continue;
+			}
+			printf("Directory:%s\n",fileName);
+			strcpy(dirsFound[dirsIterator++],fileName);
+			break;
+		}
+		else if (S_ISREG(file_info.st_mode)){
+			printf("Regular file:%s\n",fileName);
+		}
+	}//close while
+closedir(directory);
+
+	for (i = 0; i < dirsIterator;i++){
+		char *dirName = dirsFound[i];
+		if (getpid() == parentpid){
+			if (fork() == 0) //filho
+			{
+			printf("\n\nI am process %d, my parent is %d, Opening %s\n ",getpid(),getppid(),dirName);
+			char *nextDirPath = cwd;
+			strcat(nextDirPath,"/");
+			strcat(nextDirPath,dirName);
+			printf("Next dir path:%s",nextDirPath);
+			execlp("lsfind","lsfind",nextDirPath,NULL);
+			perror("execlp");
+			printf("EXEC FAILED! ABORT!\n");
 			}
 		}
-		pid_t ppid = getpid();
-		if (fork() == 0) //filho
-		{
-			printf("\n\nI am process %d, my parent is %d, Opening %s\n ",getpid(),ppid,dirName);
-			execlp("./sfind","./sfind",dirName,NULL);
-			printf("EXEC FAILED! ABORT!\n");
-		}
-		break;
-		}
 	}
-	printf("\n");
 	return 0;
 }
-//rewinddir??
-//sisdir
-
-
-//implementar exec no fim
