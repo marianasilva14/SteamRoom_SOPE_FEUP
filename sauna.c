485a86af4a9bc056a22b23f5973801c23e986ec1
João Carvalho <joaocarvalho9@gmail.com>
2017-05-14 17:33:50 +0100

    fix 4 spaces instead

--- sauna.c
+++ sauna.c
@@ -126,10 +126,9 @@ void printRegistrationMessages(Request r1){
  */
 void timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
 {
-    long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
-    result->tv_sec = diff / 1000000;
-    result->tv_usec = diff % 1000000;
-
+	long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
+	result->tv_sec = diff / 1000000;
+	result->tv_usec = diff % 1000000;
 }
 
 
